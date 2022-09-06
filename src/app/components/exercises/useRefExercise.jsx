e8ad263cda0071952e14674ea871a8cc378a5645
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2022-09-06 20:19:40 +0300

    finished a useRef task

--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useRef } from "react";
 import CollapseWrapper from "../common/collapse";
 const UseRefExercise = () => {
+    const blockRef = useRef("");
+    const handleRef = () => {
+        blockRef.current.style.cssText =
+            "color: white; height: 150; width: 80;";
+        blockRef.current.textContent = "text";
+    };
     return (
         <CollapseWrapper title="Упражнение">
             <p className="mt-3">
@@ -12,12 +18,14 @@ const UseRefExercise = () => {
                 <li>высота и ширина станут равны 150 и 80 соответственно</li>
             </ul>
             <div
+                ref={blockRef}
                 className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
                 style={{
                     height: 40,
                     width: 60,
                     color: "white"
                 }}
+                onClick={handleRef}
             >
                 <small>Блок</small>
             </div>
